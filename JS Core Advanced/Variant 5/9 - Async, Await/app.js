00583e9a2b09dde977477a69d2d7e8595c3b301c
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2024-06-13 10:34:57 +0300

    tasks 9 are ready

--- JS Core Advanced/Variant 5/9 - Async, Await/app.js
+++ JS Core Advanced/Variant 5/9 - Async, Await/app.js
@@ -3,6 +3,16 @@
 // вернуть данные. Если происходит ошибка, функция должна обработать её и вернуть
 // сообщение об ошибке.
 
-const fetchAndProcessData = () => {
-
+async function fetchAndProcessData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (!response.ok) throw new Error(`Error, status: ${response.status}`);
+        console.log(data);
+    } catch (error) {
+        console.log(`Ошибка: ${error.message}`);
+    }
 }
+
+fetchAndProcessData('https://api.example.com/data');
+fetchAndProcessData('http://ip-api.com/json');
