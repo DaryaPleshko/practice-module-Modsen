62ae09d3cb5f051fdb72a51138d3c2e06cb286ad
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2024-06-13 12:42:39 +0300

    tasks 10 are ready

--- JS Core Advanced/Variant 5/10 - Event Loop/app.js
+++ JS Core Advanced/Variant 5/10 - Event Loop/app.js
@@ -4,3 +4,20 @@
 // Promise через одну секунду c результатом 'Odd'. Если параметр возвращает четное
 // число, то верните Promise через две секунды c результатом 'Even'.
 
+const checkNumber = (data) => {
+    return new Promise((resolve, reject) => {
+        if (isNaN(data)) {
+            return reject(new Error('Error'));
+        }
+        setTimeout(() => {
+            (data % 2 === 0) ? resolve('Even') : resolve('Odd'), data % 2 === 0 ? 2000 : 1000;
+        });
+    });
+}
+
+checkNumber(4).then(result => console.log(result)).catch(error => console.error(error));
+//через 2 секунды в консоли появится 'Even'
+checkNumber(3).then(result => console.log(result)).catch(error => console.error(error));
+//через 1 секунду в консоли появится 'Odd'
+checkNumber('hello').then(result => console.log(result)).catch(error => console.error(error));
+//в консоли появится 'Error'
