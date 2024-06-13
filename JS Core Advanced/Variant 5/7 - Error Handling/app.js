3e9343c4d4d226fc2e6bd7660687de8c63e734f4
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2024-06-13 09:22:22 +0300

    tasks 7-8 are ready

--- JS Core Advanced/Variant 5/7 - Error Handling/app.js
+++ JS Core Advanced/Variant 5/7 - Error Handling/app.js
@@ -1,2 +1,10 @@
 // Напишите функцию, которая использует блок try-catch для перехвата и обработки ошибки
 // «ReferenceError» при доступе к неопределенной переменной.
+
+const accessUndefinedVariable = () => {
+    try {
+        console.log(nonExistentVariable);
+    } catch (error) {
+        console.log("Перехвачена ошибка ReferenceError: переменная не определена.");
+    }
+}
--- JS Core Advanced/Variant 5/8 - Promise/app.js
+++ JS Core Advanced/Variant 5/8 - Promise/app.js
@@ -2,3 +2,22 @@
 // так, чтобы каждый последующий промис через 3 секунды возводил в квадрат
 // результат предыдущего промиса. После окончания манипуляций выведите числовой
 // результат в консоль
+
+new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(3), 0);
+
+}).then(function (result) {
+    console.log(result); 
+    return new Promise((resolve, reject) => { 
+        setTimeout(() => resolve(result ** 2), 3000);
+    });
+
+}).then(function (result) { 
+    console.log(result); 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(result  ** 2), 3000);
+    });
+
+}).then(function (result) {
+    console.log(result); 
+});
